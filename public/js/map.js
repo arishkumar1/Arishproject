c6b532a55b51519d902e45b546f6cd4e1b2a6f6f
arishkumar1 <arishkumar484@gmail.com>
2025-10-21 21:14:39 +0530

    new map code

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken =mapToken;
+mapboxgl.accessToken =mapToken 
 
     const map = new mapboxgl.Map({
         container: 'map', // container ID
@@ -12,7 +12,7 @@ const marker = new mapboxgl.Marker({color:"red"})
         .setLngLat(listing.geometry.coordinates) //listing.geometry.coordinates
         .setPopup(new mapboxgl.Popup({offset:25}).setHTML(
         `<h4>${listing.title},${listing.location}</h4><p>Exact location will be provided after booking </p>`))
-        .addTo(map);    
+        .addTo(map);     
 
 // mapboxgl.accessToken = mapToken; // Add your Mapbox access token
 // const coordinates = [77.2088, 28.6139]; // Define coordinates for the marker
